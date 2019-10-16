3add06e92613bf0992f83baedc471b151f3cf9ad
root <root@localhost.localdomain>
2019-10-16 16:49:24 -0400

    Updated utils library

--- include/utils.h
+++ include/utils.h
@@ -6,6 +6,7 @@
 #define FAIL    1
 #define PROTO   1
 #define HW      2
+#define DEC     10
 
 void check_args(const char *msg);
 void parse_bytes(BYTE *stream, char div, const char *str, int size, int base);
