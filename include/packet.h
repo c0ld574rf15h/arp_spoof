d989cc123b17ce256124f263760f6e2e65bf497d
root <root@localhost.localdomain>
2019-10-16 20:47:00 -0400

    Added macros

--- include/packet.h
+++ include/packet.h
@@ -9,8 +9,10 @@ typedef uint32_t    DWORD;
 #define HW_ADDR_LEN     6
 #define IP_ADDR_LEN     4
 #define SZ_ETHERNET     14
+#define TYPE_ETH        0x0001
 #define TYPE_ARP        0X0806
 #define TYPE_IPV4       0x0800
+#define DONT_CARE       0
 #define OP_REQ          1
 #define OP_RES          2
 
@@ -28,9 +30,23 @@ typedef struct arp_hdr {
     BYTE trg_hw_addr[HW_ADDR_LEN], trg_proto_addr[IP_ADDR_LEN];
 } ARP;
 
+typedef struct ip_hdr {
+    BYTE ver_hdl, tos;
+    WORD ip_len;
+    WORD id, frag_off;
+    BYTE ttl, protocol, hdr_cecksum;
+    BYTE src_addr[IP_ADDR_LEN], dst_addr[IP_ADDR_LEN];
+} IP;
+
 typedef struct arp_packet {
     ETH eth_hdr;
     ARP arp_hdr;
-};
+} ARP_PKT;
+
+typedef struct ip_packet {
+    ETH eth_hdr;
+    IP  ip_hdr;
+} IP_PKT;
 
 int pcap_next_handler(int res, struct pcap_pkthdr *hdr);
+void send_arp(pcap_t *handle, int opcode, BYTE *snd_mac, BYTE *snd_ip, BYTE *trg_mac, BYTE *trg_ip);
